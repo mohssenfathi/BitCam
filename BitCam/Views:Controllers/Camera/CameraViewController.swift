df77cf7e706d9d9486e2bfddc950f6c0d0bce0ed
Mohssen Fathi <mmohssenfathi@gmail.com>
2017-01-28 13:14:28 -0600

    commit

---
+++ BitCam/Views:Controllers/Camera/CameraViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CameraViewController.swift
+//  BitCam
+//
+//  Created by Mohssen Fathi on 1/28/17.
+//  Copyright © 2017 mohssenfathi. All rights reserved.
+//
+
+import UIKit
+import MTLImage
+
+class CameraViewController: UIViewController {
+
+    @IBOutlet weak var mtlView: MTLView!
+    
+    let camera = MTLCamera()
+    let filterGroup = MTLFilterGroup()
+    let pixellate = Pixellate()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        mtlView.contentMode = .scaleAspectFit
+        camera --> filterGroup --> mtlView
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
